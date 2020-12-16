30c371c553754aeaeed5bf58ccf461f7d2203f6b
kns-1 <navyakovuru2020@gmail.com>
2020-12-16 18:33:31 +1000

    First springboot web-app demo

---
+++ springboot/demo-webapp/src/main/java/com/example/demowebapp/DemoWebappApplication.java
@@ -0,0 +1,13 @@
+package com.example.demowebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoWebappApplication.class, args);
+	}
+
+}
