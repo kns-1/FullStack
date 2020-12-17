073ab82e06ffc1128386e30595126d2a4c75908f
kns-1 <navyakovuru2020@gmail.com>
2020-12-17 13:17:36 +1000

    used @RequestParam to enter text in the URL and display it in browser for compatiability

--- springboot/springboot-webapplication/src/main/java/com/example/springbootwebapplication/controller/JspController.java
+++ springboot/springboot-webapplication/src/main/java/com/example/springbootwebapplication/controller/JspController.java
@@ -1,28 +1,28 @@
 package com.example.springbootwebapplication.controller;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 //import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class JspController {
 
     @RequestMapping("/home")
     // @ResponseBody //displays the jsp page name on the browser
-    public String jspHome(HttpServletRequest requestObj) {
+    public String jspHome(@RequestParam("myText") String myTextObj, HttpSession sessionObj) {
 
-        HttpSession sessionObj = requestObj.getSession();
 
-        String myTextObj = requestObj.getParameter("myText"); /* the text entered in the URL will be displayed.
-        URL format - localhost:8080/home?myText=welcome+blah+blah!  */
 
         System.out.println("Inside jspHome() " + myTextObj);
 
-        sessionObj.setAttribute("myText", myTextObj);
+        sessionObj.setAttribute("myText", myTextObj); 
 
+       /* Before including @RequestParam("myText"), URL format - localhost:8080/home?myTextObj=welcome+blah+blah!
+        After including @RequestParam("myText"), URL format - localhost:8080/home?myText=welcome+blah+blah! */
+        
         return "home";
     }
 
