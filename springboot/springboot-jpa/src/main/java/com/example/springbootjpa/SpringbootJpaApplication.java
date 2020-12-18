6e16f80c088990b49caf9b2eea656de70bc63a0f
kns-1 <navyakovuru2020@gmail.com>
2020-12-18 14:12:42 +1000

    removed unnecessary componentscan lines of code in main()

--- springboot/springboot-jpa/src/main/java/com/example/springbootjpa/SpringbootJpaApplication.java
+++ springboot/springboot-jpa/src/main/java/com/example/springbootjpa/SpringbootJpaApplication.java
@@ -1,14 +1,14 @@
 package com.example.springbootjpa;
 
 
-import com.example.springbootjpa.controller.JpaController;
+//import com.example.springbootjpa.controller.JpaController;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+//import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackageClasses = JpaController.class)
+//@ComponentScan(basePackageClasses = JpaController.class)
 public class SpringbootJpaApplication {
 
 	public static void main(String[] args) {
