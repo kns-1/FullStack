66dc94dc14136b59b5e4b1c06da1fabd8168fadd
kns-1 <navyakovuru2020@gmail.com>
2020-12-18 15:03:03 +1000

    get values on to browser through URL (REST Example)

--- springboot/springboot-jpa/src/main/java/com/example/springbootjpa/controller/JpaController.java
+++ springboot/springboot-jpa/src/main/java/com/example/springbootjpa/controller/JpaController.java
@@ -7,9 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
+//import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class JpaController {
