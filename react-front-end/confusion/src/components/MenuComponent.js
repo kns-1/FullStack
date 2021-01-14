d43274fff61cc9078e96da4ebc7ce2572e18fb1b
kns-1 <navyakovuru2020@gmail.com>
2021-01-14 23:01:37 +1000

    added onclick card behaviour

--- react-front-end/confusion/src/components/MenuComponent.js
+++ react-front-end/confusion/src/components/MenuComponent.js
@@ -7,7 +7,32 @@ class Menu extends Component {
     constructor(props) {
         super(props);
 
-        this.state = {}
+        this.state = {
+            selectedDish: null
+        }
+    }
+
+    onDishSelect(dish) {
+        this.setState({ selectedDish: dish });
+    }
+
+    renderDish(dish) {
+        if(dish != null) {
+            return(
+                <Card>
+                    <CardImg width="100%" src={dish.image} alt={dish.name} />
+                    <CardBody>
+                        <CardTitle>{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            );
+        }
+        else {
+            return(
+                <div></div>
+            );
+        }
     }
 
     render() {
@@ -15,7 +40,7 @@ class Menu extends Component {
         const menu = this.props.dishes.map((dish) => {
             return (
                 <div key={dish.id} className="col-12 col-md-5 m-1">
-                    <Card>
+                    <Card onClick={() => this.onDishSelect(dish)}>
                         <CardImg width="100%" src={dish.image} alt={dish.name} />
                         <CardImgOverlay>
                             <CardTitle>{dish.name}</CardTitle>
@@ -29,6 +54,9 @@ class Menu extends Component {
             <div className="container">
                 <div className="row">
                     {menu}
+                </div>
+                <div className="row">
+                    {this.renderDish(this.state.selectedDish)}
                 </div>
             </div>
         );
