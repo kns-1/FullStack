e032229de9d2d9825f52443c546494a9054c91af
kns-1 <navyakovuru2020@gmail.com>
2021-01-16 15:12:32 +1000

    added basic routingto different views like home and menu

--- react-front-end/confusion/src/App.js
+++ react-front-end/confusion/src/App.js
@@ -1,6 +1,7 @@
 import Main from './components/MainComponent';
 import './App.css';
 import { Component } from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
 class App extends Component {
 
@@ -8,9 +9,11 @@ class App extends Component {
     console.log('App.js render is invoked');
 
     return (
-      <div>
-        <Main />
-      </div>
+      <BrowserRouter>
+        <div>
+          <Main />
+        </div>
+      </BrowserRouter>
     );
   }
 }
--- react-front-end/confusion/src/components/MainComponent.js
+++ react-front-end/confusion/src/components/MainComponent.js
@@ -4,6 +4,8 @@ import { Component } from 'react';
 import DishDetail from './DishDetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import Home from './HomeComponent';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 
 class Main extends Component {
@@ -14,24 +16,41 @@ class Main extends Component {
 
         this.state = {
             dishes: DISHES,
-            selectedDish: null
+          //  selectedDish: null
         };
     }
 
-    onDishSelect(dishId) {
+   /* onDishSelect(dishId) {
         this.setState({ selectedDish: dishId });
         console.log('Main Componenet onDishSelect is invoked');
 
     }
+    */
 
     render() {
         console.log('Main Componenet render is invoked');
 
+        const HomePage = () => {
+            return(
+                <Home />
+            );
+        }
+
         return (
             <div>
                 <Header />
-                <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
+
+               {/* <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
                 <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+                */}
+
+                <Switch>
+                    <Route path="/home" component={HomePage} />
+                    <Route exact path="/menu" 
+                        component={() => <Menu dishes={this.state.dishes}/>} />
+                    
+                    <Redirect to="/home" />
+                </Switch>
                 <Footer />
             </div>
         );
