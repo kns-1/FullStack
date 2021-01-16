ce45b79dbc7381fd5f52cce2aa8a2e26a35e9b2f
kns-1 <navyakovuru2020@gmail.com>
2021-01-16 16:27:18 +1000

    single page applications, part-1: designed home component

--- react-front-end/confusion/src/components/MainComponent.js
+++ react-front-end/confusion/src/components/MainComponent.js
@@ -1,9 +1,13 @@
+import React, { Component } from 'react';
+import Home from './HomeComponent';
 import Menu from './MenuComponent';
-import { DISHES } from '../shared/dishes';
-import { Component } from 'react';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
-import Home from './HomeComponent';
+import Contact from './ContactComponent';
+import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { LEADERS } from '../shared/leaders';
+import { PROMOTIONS } from '../shared/promotions';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
 
@@ -15,7 +19,10 @@ class Main extends Component {
 
         this.state = {
             dishes: DISHES,
-           // selectedDish: null
+            comments: COMMENTS,
+            promotions: PROMOTIONS,
+            leaders: LEADERS
+            // selectedDish: null
         };
     }
 
@@ -25,14 +32,17 @@ class Main extends Component {
  
      }
      */
-     
+
 
     render() {
         console.log('Main Componenet render is invoked');
 
         const HomePage = () => {
             return (
-                <Home />
+                <Home dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+                    promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+                    leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+                />
             );
         }
 
@@ -47,6 +57,7 @@ class Main extends Component {
                 <Switch>
                     <Route path='/home' component={HomePage} />
                     <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+                    <Route exact path="/contactus" component={Contact} />
                     <Redirect to="/home" />
                 </Switch>
                 <Footer />
