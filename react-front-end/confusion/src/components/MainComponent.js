e25a48c377ca6098c9a16d59c31043bf712b93d1
kns-1 <navyakovuru2020@gmail.com>
2021-01-16 13:26:45 +1000

    added header and footer components

--- react-front-end/confusion/src/components/MainComponent.js
+++ react-front-end/confusion/src/components/MainComponent.js
@@ -1,8 +1,9 @@
-import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './MenuComponent';
 import { DISHES } from '../shared/dishes';
 import { Component } from 'react';
 import DishDetail from './DishDetailComponent';
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
 
 
 class Main extends Component {
@@ -28,13 +29,10 @@ class Main extends Component {
 
         return (
             <div>
-                <Navbar dark color="primary">
-                    <div className="container">
-                        <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-                    </div>
-                </Navbar>
+                <Header />
                 <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
                 <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+                <Footer />
             </div>
         );
     }
