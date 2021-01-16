75937ce747050251b8fa81325f591133e0140b81
kns-1 <navyakovuru2020@gmail.com>
2021-01-16 12:53:56 +1000

    implemented functional components instead of class components

--- react-front-end/confusion/src/components/DishDetailComponent.js
+++ react-front-end/confusion/src/components/DishDetailComponent.js
@@ -1,7 +1,63 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
+//functional component implementation
+function RenderDish({ dish }) {
+  if (dish != null)
+    return (
+      <div className="row">
+        <div className="col-12 col-md-5 m-1">
+          <Card>
+            <CardImg width="100%" top src={dish.image} alt={dish.name} />
+            <CardBody>
+              <CardTitle>{dish.name}</CardTitle>
+              <CardText>{dish.description}</CardText>
+            </CardBody>
+          </Card>
+        </div>
+        <div className="col-12 col-md-5 m-1">
+          <h4>Comments</h4>
+          <RenderComments comms={dish.comments} />
+        </div>
+      </div>
+    );
+  else
+    return (
+      <div></div>
+    );
+}
 
+
+function RenderComments({ comms }) {
+  return (
+    <ul className="list-unstyled">
+      {comms.map((comm) => {
+        return (
+          <li key={comm.id}>
+            <p>{comm.comment}</p>
+            <p>-- {comm.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse( comm.date )))}</p>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
+
+
+const DishDetail = (props) => {
+  return (
+    <div className="container">
+      <RenderDish dish={props.dish} />
+    </div>
+  );
+}
+
+
+export default DishDetail;
+
+
+/*
+//class component implementation
 class DishDetail extends Component {
 
   componentDidMount() {
@@ -68,3 +124,4 @@ componentDidUpdate() {
 }
 
 export default DishDetail;
+*/
--- react-front-end/confusion/src/components/MenuComponent.js
+++ react-front-end/confusion/src/components/MenuComponent.js
@@ -1,6 +1,48 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
 
+
+//1st-way of implementing functional components
+function RenderMenuItem({ dish, onClick }) {
+    return (
+        <Card onClick={() => onClick(dish.id)}>
+            <CardImg width="100%" src={dish.image} alt={dish.name} />
+            <CardImgOverlay>
+                <CardTitle>{dish.name}</CardTitle>
+            </CardImgOverlay>
+        </Card>
+    );
+}
+
+
+//2nd-way of implementing functional components
+const Menu = (props) => {
+    const menu = props.dishes.map((dish) => {
+        return (
+            <div key={dish.id} className="col-12 col-md-5 m-1">
+                <RenderMenuItem dish={dish} onClick={props.onClick}>
+
+                </RenderMenuItem>
+            </div>
+        );
+    });
+
+    return (
+        <div className="container">
+            <div className="row">
+                {menu}
+            </div>
+        </div>
+    );
+}
+
+
+
+export default Menu;
+
+
+/*
+//class component implementation
 class Menu extends Component {
 
     constructor(props) {
@@ -9,7 +51,6 @@ class Menu extends Component {
         console.log('Menu Componenet constructor is invoked');
     }
 
-  
     render() {
 
         const menu = this.props.dishes.map((dish) => {
@@ -38,3 +79,5 @@ class Menu extends Component {
 }
 
 export default Menu;
+
+*/
