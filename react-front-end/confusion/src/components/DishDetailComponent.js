3da028ba685c50d35de0641253f517dca0f43b19
kns-1 <navyakovuru2020@gmail.com>
2021-01-19 17:31:59 +1000

    fetch from server

--- react-front-end/confusion/src/components/DishDetailComponent.js
+++ react-front-end/confusion/src/components/DishDetailComponent.js
@@ -3,6 +3,7 @@ import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbIte
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 
 //functional component implementation
@@ -10,7 +11,7 @@ function RenderDish({ dish }) {
   return (
     <div className="col-12 col-md-5 m-1">
       <Card>
-        <CardImg top src={dish.image} alt={dish.name} />
+      <CardImg top src={baseUrl + dish.image} alt={dish.name} />
         <CardBody>
           <CardTitle>{dish.name}</CardTitle>
           <CardText>{dish.description}</CardText>
