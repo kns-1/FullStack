331b8c09ebae7a948a7fa4f9a046981d9ea80dcf
kns-1 <navyakovuru2020@gmail.com>
2021-01-19 14:47:12 +1000

    react-redux-form revisited

--- react-front-end/confusion/src/redux/configureStore.js
+++ react-front-end/confusion/src/redux/configureStore.js
@@ -3,6 +3,8 @@ import { Dishes } from './dishes';
 import { Comments } from './comments';
 import { Leaders } from './leaders';
 import { Promotions } from './promotions';
+import { createForms } from 'react-redux-form';
+import { InitialFeedback } from './forms';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 
@@ -13,7 +15,10 @@ export const ConfigureStore = () => {
             dishes: Dishes,
             comments: Comments,
             promotions: Promotions,
-            leaders: Leaders
+            leaders: Leaders,
+            ...createForms({
+                feedback: InitialFeedback
+            })
         }),
         
         applyMiddleware(thunk, logger)
