378d4a019da38a394da3eb63a35c14c77661abf0
kns-1 <navyakovuru2020@gmail.com>
2021-02-15 12:53:12 +1000

    assignment-1 tasks-2,3 completed

--- mern/node-express/index.js
+++ mern/node-express/index.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser');
 
 
 const dishRouter = require('./routes/dishRouter');
+const promoRouter = require('./routes/promoRouter');
+const leaderRouter = require('./routes/leaderRouter');
 const hostname = 'localhost';
 const port = 3000;
 
@@ -13,6 +15,8 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 
 app.use('/dishes', dishRouter);
+app.use('/promotions', promoRouter);
+app.use('/leaders', leaderRouter);
 app.use(express.static(__dirname + '/public'));
 
 
