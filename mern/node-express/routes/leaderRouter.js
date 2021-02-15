48fb58db644b490e252a495f1e77c2bc45ef83de
kns-1 <navyakovuru2020@gmail.com>
2021-02-15 12:56:50 +1000

    completed assignment-1

--- mern/node-express/routes/leaderRouter.js
+++ mern/node-express/routes/leaderRouter.js
@@ -15,7 +15,7 @@ leaderRouter.route('/')
     res.end('Will send all the leaders to you!');
 })
 .post((req, res, next) => {
-    res.end('Will add the promo: ' + req.body.name + ' with details: ' + req.body.description);
+    res.end('Will add the leader: ' + req.body.name + ' with details: ' + req.body.description);
 })
 .put((req, res, next) => {
     res.statusCode = 403;
@@ -33,19 +33,19 @@ leaderRouter.route('/:leaderId')
     next();
 })
 .get((req,res,next) => {
-    res.end('Will send details of the promo: ' + req.params.leaderId + ' to you!');
+    res.end('Will send details of the leader: ' + req.params.leaderId + ' to you!');
 })
 .post((req, res, next) => {
     res.statusCode = 403;
     res.end('POST operation not supported on /leaders/' + req.params.leaderId);
 })
 .put((req, res, next) => {
-    res.write('Updating the promo: ' + req.params.leaderId + '\n');
-    res.end('Will update the promo: ' + req.body.name +
+    res.write('Updating the leader: ' + req.params.leaderId + '\n');
+    res.end('Will update the leader: ' + req.body.name +
         ' with details: ' + req.body.description);
 })
 .delete((req, res, next) => {
-    res.end('Deleting promo: ' + req.params.leaderId);
+    res.end('Deleting leader: ' + req.params.leaderId);
 });
     
 
