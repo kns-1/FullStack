1e19244c11833779fb864b97c80960833f761e80
kns-1 <navyakovuru2020@gmail.com>
2021-02-24 12:50:55 +1100

    pushed sub-documents (ex. comments) into dish and printed result

--- mern/node-mongoose/index.js
+++ mern/node-mongoose/index.js
@@ -15,22 +15,38 @@ connect.then((db) => {
         description: 'test'
     })//;
 
-    //newDish.save()
-    .then((dish) => {
-        console.log(dish);
+        //newDish.save()
+        .then((dish) => {
+            console.log(dish);
 
-        return Dishes.find({});
-    })
-    .then((dishes) => {
-        console.log(dishes);
+            return Dishes.findByIdAndUpdate(dish._id, {
+                $set: { description: 'Updated test' }
+            }, {
+                new: true
+            })
+                .exec();
+        })
+        .then((dish) => {
+            console.log(dish);
 
-        return Dishes.remove({});
-    })
-    .then(() => {
-        return mongoose.connection.close();
-    })
-    .catch((err) => {
-        console.log(err);
-    });
+            dish.comments.push({
+                rating: 5,
+                comment: 'I\'m getting a sinking feeling!',
+                author: 'Leonardo di Carpaccio'
+            });
+
+            return dish.save();
+        })
+        .then((dishes) => {
+            console.log(dishes);
+
+            return Dishes.remove({});
+        })
+        .then(() => {
+            return mongoose.connection.close();
+        })
+        .catch((err) => {
+            console.log(err);
+        });
 
 });
