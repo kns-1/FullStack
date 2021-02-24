0ddb77578f712f2d4c9c6d654cd67708528c39e2
kns-1 <navyakovuru2020@gmail.com>
2021-02-24 12:43:12 +1100

    mongoose odm modified

--- mern/node-mongoose/index.js
+++ mern/node-mongoose/index.js
@@ -9,27 +9,28 @@ connect.then((db) => {
 
     console.log('Connected correctly to server');
 
-    var newDish = Dishes({
+    // var newDish = Dishes({
+    Dishes.create({
         name: 'Uthappizza',
         description: 'test'
+    })//;
+
+    //newDish.save()
+    .then((dish) => {
+        console.log(dish);
+
+        return Dishes.find({});
+    })
+    .then((dishes) => {
+        console.log(dishes);
+
+        return Dishes.remove({});
+    })
+    .then(() => {
+        return mongoose.connection.close();
+    })
+    .catch((err) => {
+        console.log(err);
     });
-
-    newDish.save()
-        .then((dish) => {
-            console.log(dish);
-
-            return Dishes.find({});
-        })
-        .then((dishes) => {
-            console.log(dishes);
-
-            return Dishes.remove({});
-        })
-        .then(() => {
-            return mongoose.connection.close();
-        })
-        .catch((err) => {
-            console.log(err);
-        });
 
 });
