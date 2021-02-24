0570094b4f0a75afbb53558e29e91a71d414c48a
kns-1 <navyakovuru2020@gmail.com>
2021-02-24 12:14:13 +1100

    final commits and node mongoose odm demo

--- mern/node-mongo/index.js
+++ mern/node-mongo/index.js
@@ -5,37 +5,79 @@ const dboper = require('./operations');
 const url = 'mongodb://localhost:27017/';
 const dbname = 'conFusion';
 
-MongoClient.connect(url, (err, client) => {
-
-    assert.equal(err, null);
+//Promises code
+MongoClient.connect(url).then((client) => {
 
     console.log('Connected correctly to server');
-
     const db = client.db(dbname);
+
+    dboper.insertDocument(db, { name: "Vadonut", description: "Test"},
+        "dishes")
+        .then((result) => {
+            console.log("Insert Document:\n", result.ops); //ops tells number of insert operations carried out
+
+            return dboper.findDocuments(db, "dishes");
+        })
+        .then((docs) => {
+            console.log("Found Documents:\n", docs);
+
+            return dboper.updateDocument(db, { name: "Vadonut" },
+                    { description: "Updated Test" }, "dishes");
+
+        })
+        .then((result) => {
+            console.log("Updated Document:\n", result.result);
+
+            return dboper.findDocuments(db, "dishes");
+        })
+        .then((docs) => {
+            console.log("Found Updated Documents:\n", docs);
+                            
+            return db.dropCollection("dishes");
+        })
+        .then((result) => {
+            console.log("Dropped Collection: ", result);
+
+            return client.close();
+        })
+        .catch((err) => console.log(err));
+
+})
+.catch((err) => console.log(err));
+
+
+//Callback Hell problem with this code. Hence, remove call backs from operations.js and use Promises in index.js to resolve it and handle errors (implemented in above working code)
+// MongoClient.connect(url, (err, client) => {
+
+//     assert.equal(err, null);
+
+//     console.log('Connected correctly to server');
+
+//     const db = client.db(dbname);
     
-    dboper.insertDocument(db, { name: "Vadonut", description: "Test" },
-        "dishes", (result) => {
-            console.log("Insert Document:\n", result.ops);
+//     dboper.insertDocument(db, { name: "Vadonut", description: "Test" },
+//         "dishes", (result) => {
+//             console.log("Insert Document:\n", result.ops);
 
-            dboper.findDocuments(db, "dishes", (docs) => {
-                console.log("Found Documents:\n", docs);
+//             dboper.findDocuments(db, "dishes", (docs) => {
+//                 console.log("Found Documents:\n", docs);
 
-                dboper.updateDocument(db, { name: "Vadonut" },
-                    { description: "Updated Test" }, "dishes",
-                    (result) => {
-                        console.log("Updated Document:\n", result.result);
+//                 dboper.updateDocument(db, { name: "Vadonut" },
+//                     { description: "Updated Test" }, "dishes",
+//                     (result) => {
+//                         console.log("Updated Document:\n", result.result);
 
-                        dboper.findDocuments(db, "dishes", (docs) => {
-                            console.log("Found Updated Documents:\n", docs);
+//                         dboper.findDocuments(db, "dishes", (docs) => {
+//                             console.log("Found Updated Documents:\n", docs);
 
-                            db.dropCollection("dishes", (result) => {
-                                console.log("Dropped Collection: ", result);
+//                             db.dropCollection("dishes", (result) => {
+//                                 console.log("Dropped Collection: ", result);
 
-                                client.close();
-                            });
-                        });
-                    });
-            });
-        });
+//                                 client.close();
+//                             });
+//                         });
+//                     });
+//             });
+//         });
 
-});
+// });
---
+++ mern/node-mongoose/Readme.md
@@ -0,0 +1,2 @@
+>npm init
+npm install mongoose@5.1.7 --save
